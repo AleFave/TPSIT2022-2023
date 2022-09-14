c8f3cab758c94ccd3bcc8446d681b46646a06e65
AleFive <quintanialex@gmail.com>
2022-09-14 12:27:58 +0200

    chat UDP with client

--- ServerEClient/client.py
+++ ServerEClient/client.py
@@ -1,11 +1,14 @@
 from socket import AF_INET, SOCK_DGRAM, SOCK_RDM, socket
 
+PORT = int(input("\nInserisci la porta (consigliato 5000) "))
+
 def chatClient():
     with socket(AF_INET, SOCK_DGRAM) as s:
-        msg = "hello world"
-        msg = msg.encode('utf8')
+        while True:
+            msg = input("Inserisci il messaggio da inviare: ")
+            msg = msg.encode('utf8')
 
-        s.sendto(msg, ("192.168.88.255", 5000)) #.255 perchè è il broadcast
+            s.sendto(msg, ("192.168.112.255", PORT)) #.255 perchè è il broadcast + inserire proprio IP
 
 if __name__ == "__main__":
     chatClient()
--- ServerEClient/server.py
+++ ServerEClient/server.py
@@ -6,21 +6,26 @@ mystr = "ciao"
 #In python le stringhe si chiamano str, non sono array di byte, con l'istruzione bytes converto la stringa in byte
 #Posso usare encode e decode per convertire in byte e non byte
 
-HOST = "0.0.0.0"
-PORT = 5000
+HOST = input("\nInserisci host (consigliato 0.0.0.0) ")
+PORT = int(input("\nInserisci la porta (consigliato 5000) "))
 #Possibilità host:
     #Localhost: 127.0.0.1
     #Interfaccia particolare
     #Ascoltare dati da qualsiasi interfaccia (0.0.0.0)
+file = open("frasi.csv", "a")
 
 def chatServer():
     #In questo modo apro un oggetto socket chiamato s, questo s si usa solo nel blocco with, una volta usciti da with chiudo automaticamente la risorsa
     with socket(AF_INET, SOCK_DGRAM) as s: #AF_INET = Definisce che uso IP di v4 (IPv4), SOCK_DGRAM si usa per inviare pacchetti
+        print("\nServer in ascolto")
         s.bind((HOST,PORT)) #bind ci mette in ascolto
-        msg = s.recvfrom(BUFFER_SIZE) #recvfrom fa in modo che la variabile riceva il messaggio
-        print(msg)
-        msg = msg[0].decode('utf8')
-        print(msg)
+        while True:
+            msg = s.recvfrom(BUFFER_SIZE) #recvfrom fa in modo che la variabile riceva il messaggio
+            msg = msg[0].decode('utf8')
+            print(msg)
+            file = open("frasi.csv", "a")
+            file.write(msg+"\n")
+            file.close()
 
 if __name__ == "__main__":
     chatServer()
