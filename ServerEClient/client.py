e04305fcac69f737e3ac627d147d585f979b07e9
AleFive <quintanialex@gmail.com>
2022-11-14 12:17:47 +0100

    Classe per Server e Client

--- ServerEClient/client.py
+++ ServerEClient/client.py
@@ -1,14 +1,14 @@
-from socket import AF_INET, SOCK_DGRAM, SOCK_RDM, socket
 
-PORT = int(input("\nInserisci la porta (consigliato 5000) "))
+from socket import AF_INET, SOCK_DGRAM, SOCK_RDM, socket
 
 def chatClient():
     with socket(AF_INET, SOCK_DGRAM) as s:
+        PORT = int(input("\nInserisci la porta (consigliato 5000) "))
         while True:
             msg = input("Inserisci il messaggio da inviare: ")
             msg = msg.encode('utf8')
 
-            s.sendto(msg, ("192.168.112.255", PORT)) #.255 perchè è il broadcast + inserire proprio IP
+            s.sendto(msg, ("192.168.0.255", PORT)) #.255 perchè è il broadcast + inserire proprio IP
 
 if __name__ == "__main__":
     chatClient()
--- ServerEClient/server.py
+++ ServerEClient/server.py
@@ -6,8 +6,6 @@ mystr = "ciao"
 #In python le stringhe si chiamano str, non sono array di byte, con l'istruzione bytes converto la stringa in byte
 #Posso usare encode e decode per convertire in byte e non byte
 
-HOST = input("\nInserisci host (consigliato 0.0.0.0) ")
-PORT = int(input("\nInserisci la porta (consigliato 5000) "))
 #Possibilità host:
     #Localhost: 127.0.0.1
     #Interfaccia particolare
@@ -17,8 +15,11 @@ file = open("frasi.csv", "a")
 def chatServer():
     #In questo modo apro un oggetto socket chiamato s, questo s si usa solo nel blocco with, una volta usciti da with chiudo automaticamente la risorsa
     with socket(AF_INET, SOCK_DGRAM) as s: #AF_INET = Definisce che uso IP di v4 (IPv4), SOCK_DGRAM si usa per inviare pacchetti
+        HOST = "0.0.0.0"
+        PORT = int(input("\nInserisci la porta (consigliato 5000) "))
         print("\nServer in ascolto")
         s.bind((HOST,PORT)) #bind ci mette in ascolto
+        
         while True:
             msg = s.recvfrom(BUFFER_SIZE) #recvfrom fa in modo che la variabile riceva il messaggio
             msg = msg[0].decode('utf8')
